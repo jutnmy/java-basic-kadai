0f92d332e833a6e563b13b4e8c73606cb311f423
jutnmy <89903647+jutnmy@users.noreply.github.com>
2025-05-12 15:45:31 +0900

    提出2回目

--- kadai_021/src/kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/src/kadai_021/DictionaryExec_Chapter21.java
@@ -1,12 +1,21 @@
 package kadai_021;
 
+import java.util.ArrayList;
+
 public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
 		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
 		
-		dictionary.search("apple");
-		dictionary.search("banana");
-		dictionary.search("grape");
-		dictionary.search("orange");
+		// 調べる対象を配列で管理
+		ArrayList<String> fruitList = new ArrayList<String>();
+		fruitList.add("apple");
+		fruitList.add("banana");
+		fruitList.add("grape");
+		fruitList.add("orange");
+		
+		for (String fruit : fruitList) {
+			dictionary.search(fruit);
+		}
+		
 	}
 }
